77cca065b3c696e8a9f0ab94c68fc0a04ecb05c9
codewaster <codewaster@bitbucket.org>
2018-03-13 14:50:20 +0700

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBottom() {
+  var messages = $("#messages");
+  var newMessage = messages.children("li:last-child");
+
+  var clientHeight = messages.prop("clientHeight");
+  var scrollTop = messages.prop("scrollTop");
+  var scrollHeight = messages.prop("scrollHeight");
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on("connect", () => {
   console.log("Conected to server");
 });
@@ -18,6 +36,7 @@ socket.on("newMessage", function(msg) {
   });
 
   $("#messages").append(html);
+  scrollToBottom();
 });
 
 socket.on("newLocationMessage", function(message) {
@@ -31,6 +50,7 @@ socket.on("newLocationMessage", function(message) {
   });
 
   $("#messages").append(html);
+  scrollToBottom();
 });
 
 $("#message-form").on("submit", function(e) {
